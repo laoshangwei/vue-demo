0b3fa815b77288bd8e751578679b81547dad13b9
R <shangwei.lao@gmail.com>
2019-05-30 16:48:50 +0700

    update

---
+++ telegram.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const Telegram = require("telegram-node-bot");
+const TelegramBaseController = Telegram.TelegramBaseController;
+const TextCommand = Telegram.TextCommand;
+const tg = new Telegram.Telegram(
+  "592844526:AAFV_ljxoDRaDoTfN95gNpbXpEetjR8wzXc"
+);
+
+class PingController extends TelegramBaseController {
+  /**
+   * @param {Scope} $
+   */
+  pingHandler($) {
+    $.sendMessage("pong");
+  }
+
+  get routes() {
+    return {
+      pingCommand: "pingHandler"
+    };
+  }
+}
+
+class StartConstoller extends TelegramBaseController {
+  /**
+   * @param {Scope} $
+   */
+  start($) {
+    $.sendMessage("Hello!");
+  }
+
+  get routes() {
+    return {
+      startHandler: "start"
+    };
+  }
+}
+
+tg.router
+  .when(new TextCommand("ping", "pingCommand"), new PingController())
+  .when(new TextCommand("/start", "startHandler"), new StartConstoller());
